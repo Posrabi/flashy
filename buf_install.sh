b4a1b9e660faec81a1a6aa1a4e8e2e6b3a13a32a
tridao <tridao@mitofthenorth.com>
2022-03-02 18:33:36 -0400

    install buf in actions

---
+++ buf_install.sh
@@ -0,0 +1,8 @@
+# Substitute BIN for your bin directory.
+# Substitute VERSION for the current released version.
+BIN="/usr/local/bin" && \
+VERSION="1.1.0" && \
+  curl -sSL \
+    "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m)" \
+    -o "${BIN}/buf" && \
+  chmod +x "${BIN}/buf"
