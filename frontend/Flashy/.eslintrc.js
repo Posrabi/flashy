cb68f8790e30a30643627cbd1fd07b4974913895
tridao <tridao@mitofthenorth.com>
2022-03-02 23:05:45 -0400

    recoil right place

--- frontend/Flashy/.eslintrc.js
+++ frontend/Flashy/.eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react-hooks'],
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': [
+            'warn',
+            {
+                additionalHooks: '(useRecoilCallback|useRecoilTransaction_UNSTABLE)',
+            },
+        ],
     },
-  ],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+            },
+        },
+    ],
 };
