a7dfe17977b825b827acf716e1bfba114267db5f
tridao <tridao@mitofthenorth.com>
2022-03-08 22:35:27 -0400

    Revert "abandoning react native"
    This reverts commit 9ef7fc1e640d1c676b46eeba1229d48084162685.

---
+++ frontend/Flashy/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['eslint-plugin-tsdoc', 'react-hooks'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        '@react-native-community',
+        'universe',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    rules: {
+        'func-style': ['error', 'expression'],
+        '@typescript-eslint/typedef': [
+            'error',
+            {
+                arrowParameter: true,
+                parameter: true,
+            },
+        ],
+        '@typescript-eslint/no-inferrable-types': [
+            'error',
+            {
+                ignoreParameters: true,
+            },
+        ],
+        // React 17 no longer requires react in scope for JSX
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+        // "tsdoc/syntax": "warn"
+    },
+};
---
+++ frontend/Flashy/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 4,
+  semi: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+};
