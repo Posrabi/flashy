30219420d0291139fd10e6c242b678c107653198
tridao <tridao@mitofthenorth.com>
2022-03-09 10:03:32 -0400

    Revert "going swift all the way"
    This reverts commit 0c85b3c1f60bf542c3a5f6a65046a85ed6f57a54.

--- build_protos.sh
+++ build_protos.sh
@@ -8,5 +8,7 @@ yarn
 
 echo "[STARTING] - Generating go and ts types"
 buf generate -o protos --template buf.gen.yaml
+buf generate -o protos --template buf_java.gen.yaml
 cp protos/users/proto/users.ts frontend/Flashy/src/types/flashy.ts
+cp protos/com/flashy/Users.java frontend/Flashy/android/app/src/main/java/com/flashy/Users.java
 echo "[FINISHED] - Generating go and ts types"
